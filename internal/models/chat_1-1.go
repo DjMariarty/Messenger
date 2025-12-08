2bdf929a9d256f5cd0509c8dcd34367f1900f2c8
Bekkhanbs <bekkhanbs@gmail.com>
2025-12-08 14:36:33 +0300

    сделал структуру

--- internal/models/chat_1-1.go
+++ internal/models/chat_1-1.go
@@ -8,8 +8,8 @@ import (
 
 type User struct {
 	gorm.Model
-	Username string
-	Online   bool
+	Username   string
+	Online     bool
 	LastSeenAt *time.Time
 	Typing     bool
 }
@@ -17,18 +17,18 @@ type User struct {
 type Chat struct {
 	gorm.Model
 	User1ID uint
-	User2ID uint 
+	User2ID uint
 }
 
 type Message struct {
 	gorm.Model
-	ChatID   uint
-	SenderID uint
-	Type     string
-	Text     string
-	VoiceURL string
-	Edited   bool
-	Deleted   bool
-	Status   string
+	ChatID      uint
+	SenderID    uint
+	Type        string
+	Text        string
+	VoiceURL    string
+	Edited      bool
+	Deleted     bool
+	Status      string
 	DurationSec int
 }
