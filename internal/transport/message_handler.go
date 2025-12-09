7e703ebc91db7491bf8644560e24f24a67d51d55
Bekkhanbs <bekkhanbs@gmail.com>
2025-12-09 14:35:08 +0300

    сделал транспорт

--- internal/transport/message_handler.go
+++ internal/transport/message_handler.go
@@ -1,1 +1,32 @@
 package transport
+
+import (
+	"net/http"
+
+	"github.com/DjMariarty/messenger/internal/models"
+	"github.com/DjMariarty/messenger/internal/services"
+	"github.com/gin-gonic/gin"
+)
+
+type MessageHandler struct {
+	service services.MessageService
+}
+
+func NewMessageHandler(service services.MessageService) *MessageHandler {
+	return &MessageHandler{service: service}
+}
+
+func (h *MessageHandler) CreateMessage(c *gin.Context) {
+	var req models.CreateMessageRequest
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	msg, err := h.service.CreateMessage(req)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusCreated, msg)
+}
